789ca7ba048c1b4abf163f1355988997fd209fe5
benjaminbis <benjaminbismuth2@gmail.com>
2024-11-20 23:40:44 +0200

    Update Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -5,22 +5,30 @@ public class Collatz {
 
         for (int seed = 1; seed <= maxSeed; seed++) {
             int n = seed;
-            int steps = 1;
+            int steps = 0;
 
             if (mode.equals("v")) {
                 System.out.print(n);
             }
 
-            while (n != 1) {
-                if (n % 2 == 0) {
-                    n = n / 2;
-                } else {
-                    n = 3 * n + 1;
+            if (n == 1) {
+                steps = 4;
+                if (mode.equals("v")) {
+                    System.out.print(" 4 2 1");
+                }
+            } else {
+                while (n != 1) {
+                    if (n % 2 == 0) {
+                        n = n / 2;
+                    } else {
+                        n = 3 * n + 1;
+                    }
+                    steps++;
+                    if (mode.equals("v")) {
+                        System.out.print(" " + n);
+                    }
                 }
                 steps++;
-                if (mode.equals("v")) {
-                    System.out.print(" " + n);
-                }
             }
 
             if (mode.equals("v")) {
