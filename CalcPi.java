52cdf96bafd1683cb5f381078b7f795ed359e920
benjaminbis <benjaminbismuth2@gmail.com>
2024-11-20 23:09:31 +0200

    Update CalcPi.java

--- CalcPi.java
+++ CalcPi.java
@@ -1,17 +1,18 @@
 public class CalcPi {
     public static void main(String[] args) {
+       
         int terms = Integer.parseInt(args[0]); 
         double sum = 0.0; 
 
         for (int i = 0; i < terms; i++) {
+           
             double term = Math.pow(-1, i) / (2 * i + 1);
             sum += term; 
         }
 
         double piApproximation = 4 * sum;
-        
-        System.out.println("Pi according to Java: " + Math.PI);
-        System.out.println("Pi, approximated:     " + piApproximation);
 
-    }
+        System.out.println("pi according to Java: %.15f" + piApproximation);
+        System.out.println("pi, approximated: %.15f" + Math.PI);
+    } 
 }
